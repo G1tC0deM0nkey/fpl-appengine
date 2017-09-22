b0333ca9292abf2fcb8b35dd783f63c5263cc1c8
John Kaye <jkaye@williamhill.co.uk>
2017-09-22 12:43:49 +0100

    Added update scheduling

--- fplscores/src/main/java/com/wh/fpl/FPLApplication.java
+++ fplscores/src/main/java/com/wh/fpl/FPLApplication.java
@@ -3,9 +3,11 @@ package com.wh.fpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableScheduling
 public class FPLApplication {
 
     public static void main(String [] args) {
---
+++ fplscores/src/main/java/com/wh/fpl/task/GameweekUpdateTask.java
@@ -0,0 +1,44 @@
+package com.wh.fpl.task;
+
+import com.wh.fpl.FPLApplicationConfig;
+import com.wh.fpl.control.CheckGameweek;
+import com.wh.fpl.core.FSContext;
+import com.wh.fpl.core.Gameweek;
+import com.wh.fpl.core.GameweekContext;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class GameweekUpdateTask {
+
+    private static final Logger LOG = Logger.getLogger(GameweekUpdateTask.class);
+
+    @Autowired
+    private FPLApplicationConfig config;
+
+    @Autowired
+    private GameweekContext gameweekContext;
+
+    @Scheduled(fixedRate=900000)
+    public void updateGameweek() {
+
+        try {
+            LOG.info("Updating active gameweek");
+
+            if (gameweekContext != null && gameweekContext.getActiveGameweek() != null) {
+
+                FSContext fs = new FSContext(config.getDataRoot());
+
+                CheckGameweek checkGameweek = new CheckGameweek(gameweekContext.getGameMonth(), gameweekContext.getGameWeek());
+                Gameweek gw = checkGameweek.update(gameweekContext.getActiveGameweek());
+                fs.storeGameweek(gw);
+                gameweekContext.setActiveGameweek(gw);
+
+            }
+        } catch (Exception e) {
+            LOG.error("Failed to update active gameweek", e);
+        }
+
+    }
+
+}
