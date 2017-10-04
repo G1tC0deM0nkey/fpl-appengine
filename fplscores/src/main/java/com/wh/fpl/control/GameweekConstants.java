61fc75d9111ee5df2d5148fd1e596e97df658988
John Kaye <jkaye@williamhill.co.uk>
2017-10-04 09:37:39 +0100

    Gameweek updates

--- fplscores/src/main/java/com/wh/fpl/control/GameweekConstants.java
+++ fplscores/src/main/java/com/wh/fpl/control/GameweekConstants.java
@@ -5,8 +5,8 @@ package com.wh.fpl.control;
  */
 public class GameweekConstants {
 
-    public static final int MONTH = 2;
+    public static final int MONTH = 3;
 
-    public static final int WEEK = 7;
+    public static final int WEEK = 8;
 
 }
